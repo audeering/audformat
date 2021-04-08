15f5a94590195b25f3de6a47964f62b3971ffc18
Johannes Wagner <jwagner@audeering.com>
2021-04-08 16:22:13 +0200

    Option to replace NaT with file duration when converting to segmented index

--- audformat/core/utils.py
+++ audformat/core/utils.py
@@ -1,3 +1,4 @@
+import errno
 import os
 import typing as typing
 
@@ -521,12 +522,30 @@ def to_filewise_index(
 
 def to_segmented_index(
         obj: typing.Union[pd.Index, pd.Series, pd.DataFrame],
+        *,
+        allow_nat: bool = True,
+        root: str = None,
 ) -> typing.Union[pd.Index, pd.Series, pd.DataFrame]:
     r"""Convert to segmented index.
+
+    If the input a filewise table,
+    ``start`` and ``end`` will be added as new levels to the index.
+    By default, ``start`` will be set to 0 and ``end`` to ``NaT``.
+
+    If ``allow_nat`` is set to ``False``,
+    all occurrences of ``end=NaT``
+    are replaced with the duration of the file.
+    This, however, requires that the referenced file exists.
+    If file names in the index are relative,
+    the ``root`` argument can be used to provide
+    the location where the files are stored.
 
     Args:
         obj: object conform to
             :ref:`table specifications <data-tables:Tables>`
+        allow_nat: if set to ``False``, ``end=NaT`` is replaced with file
+            duration
+        root: root directory to files in the index
 
     Returns:
         object with segmented index
@@ -534,9 +553,12 @@ def to_segmented_index(
     Raises:
         ValueError: if object not conform to
             :ref:`table specifications <data-tables:Tables>`
+        FileNotFoundError: if file is not found
 
     """
-    if index_type(obj) == define.IndexType.SEGMENTED:
+    is_segmented = index_type(obj) == define.IndexType.SEGMENTED
+
+    if is_segmented and allow_nat:
         return obj
 
     if isinstance(obj, (pd.Series, pd.DataFrame)):
@@ -544,17 +566,43 @@ def to_segmented_index(
     else:
         index = obj
 
-    index = segmented_index(
-        files=list(index),
-        starts=[0] * len(index),
-        ends=[pd.NaT] * len(index),
-    )
+    if not is_segmented:
+        index = segmented_index(
+            files=list(index),
+            starts=[0] * len(index),
+            ends=[pd.NaT] * len(index),
+        )
+
+    if not allow_nat:
+        ends = index.get_level_values(define.IndexField.END)
+        has_nat = pd.isna(ends)
+        if any(has_nat):
+            files = index.get_level_values(define.IndexField.FILE)
+            starts = index.get_level_values(define.IndexField.START)
+            ends = list(ends)
+            for idx_nat in np.where(has_nat)[0]:
+                path = files[idx_nat]
+                if root is not None and not os.path.isabs(path):
+                    path = os.path.join(root, path)
+                path = audeer.safe_path(path)
+                if not os.path.exists(path):
+                    raise FileNotFoundError(
+                        errno.ENOENT,
+                        os.strerror(errno.ENOENT),
+                        path,
+                    )
+                ends[idx_nat] = pd.to_timedelta(
+                    audiofile.duration(path),
+                    unit='s',
+                )
+            index = segmented_index(files, starts, ends)
 
     if isinstance(obj, pd.Index):
         return index
 
     obj = obj.reset_index(drop=True)
     obj.index = index
+
     return obj
 
 
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,27 @@
+import os
+import shutil
+
+import pandas as pd
 import pytest
 
 import audformat.testing
 
 
+pytest.ROOT = os.path.dirname(os.path.realpath(__file__))
+
 pytest.DB = audformat.testing.create_db()
+pytest.DB_ROOT = os.path.join(pytest.ROOT, 'db')
+pytest.DB.save(pytest.DB_ROOT)
+
+pytest.FILE_DUR = pd.to_timedelta('1s')
+
+
+@pytest.fixture(scope='session', autouse=True)
+def create_audio_files():
+    audformat.testing.create_audio_files(
+        pytest.DB,
+        root=pytest.DB_ROOT,
+        file_duration=pytest.FILE_DUR,
+    )
+    yield
+    shutil.rmtree(pytest.DB_ROOT)
