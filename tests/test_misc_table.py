51711e71f3c8db2e53879a49e8abc9bdde285535
Hagen Wierstorf <hwierstorf@audeering.com>
2022-07-20 10:44:11 +0200

    Add more tests

--- tests/test_misc_table.py
+++ tests/test_misc_table.py
@@ -35,6 +35,141 @@ def test_copy(table):
 
 
 @pytest.mark.parametrize(
+    'column_values, column_dtype, '
+    'expected_pandas_dtype, expected_audformat_dtype',
+    [
+        (
+            [],
+            None,
+            'object',
+            audformat.define.DataType.STRING,
+        ),
+        (
+            [],
+            'datetime64[ns]',
+            'datetime64[ns]',
+            audformat.define.DataType.DATE,
+        ),
+        (
+            [],
+            float,
+            'float64',
+            audformat.define.DataType.FLOAT,
+        ),
+        (
+            [],
+            int,
+            'Int64',
+            audformat.define.DataType.INTEGER,
+        ),
+        (
+            [],
+            'int64',
+            'Int64',
+            audformat.define.DataType.INTEGER,
+        ),
+        (
+            [],
+            'Int64',
+            'Int64',
+            audformat.define.DataType.INTEGER,
+        ),
+        (
+            [],
+            str,
+            'object',
+            audformat.define.DataType.STRING,
+        ),
+        (
+            [],
+            'timedelta64[ns]',
+            'timedelta64[ns]',
+            audformat.define.DataType.TIME,
+        ),
+        (
+            [0],
+            'datetime64[ns]',
+            'datetime64[ns]',
+            audformat.define.DataType.DATE,
+        ),
+        (
+            [0.0],
+            None,
+            'float64',
+            audformat.define.DataType.FLOAT,
+        ),
+        (
+            [0],
+            None,
+            'Int64',
+            audformat.define.DataType.INTEGER,
+        ),
+        (
+            [np.NaN],
+            'Int64',
+            'Int64',
+            audformat.define.DataType.INTEGER,
+        ),
+        (
+            [0, np.NaN],
+            'Int64',
+            'Int64',
+            audformat.define.DataType.INTEGER,
+        ),
+        (
+            [np.NaN],
+            'Int64',
+            'Int64',
+            audformat.define.DataType.INTEGER,
+        ),
+        (
+            ['0'],
+            None,
+            'object',
+            audformat.define.DataType.STRING,
+        ),
+        (
+            [0],
+            'timedelta64[ns]',
+            'timedelta64[ns]',
+            audformat.define.DataType.TIME,
+        ),
+    ]
+)
+def test_dtype_column(
+        tmpdir,
+        column_values,
+        column_dtype,
+        expected_pandas_dtype,
+        expected_audformat_dtype,
+):
+
+    name = 'column'
+    y = pd.Series(column_values, dtype=column_dtype, name=name)
+
+    name = 'idx'
+    index_values = [f'f{n}' for n in range(len(column_values))]
+    index = pd.Index(index_values, dtype='str', name=name)
+
+    db = audformat.testing.create_db(minimal=True)
+    db['misc'] = audformat.MiscTable(index)
+    db.schemes['column'] = audformat.Scheme(expected_audformat_dtype)
+    db['misc']['column'] = audformat.Column(scheme_id='column')
+    db['misc']['column'].set(y.values)
+
+    assert db['misc']['column'].scheme.dtype == expected_audformat_dtype
+    assert db['misc'].df['column'].dtype == expected_pandas_dtype
+
+    # Store and load table
+    db_root = tmpdir.join('db')
+    db.save(db_root, storage_format='csv')
+    db_new = audformat.Database.load(db_root)
+
+    assert db_new['misc']['column'].scheme.dtype == expected_audformat_dtype
+    assert db_new['misc'].df['column'].dtype == expected_pandas_dtype
+
+
+@pytest.mark.parametrize(
     'index_object, index_values, index_dtype, '
     'expected_pandas_dtype, expected_audformat_dtype',
     [
@@ -152,7 +287,7 @@ def test_copy(table):
         ),
     ]
 )
-def test_dtype(
+def test_dtype_index(
         tmpdir,
         index_object,
         index_values,
@@ -179,6 +314,136 @@ def test_dtype(
     db_new = audformat.Database.load(db_root)
     assert db_new['misc'].levels[name] == expected_audformat_dtype
     assert db_new['misc'].index.dtype == expected_pandas_dtype
+
+
+@pytest.mark.parametrize(
+    'index_values, index_dtype, '
+    'expected_pandas_dtype, expected_audformat_dtype',
+    [
+        (
+            [],
+            'datetime64[ns]',
+            'datetime64[ns]',
+            audformat.define.DataType.DATE,
+        ),
+        (
+            [],
+            float,
+            'float64',
+            audformat.define.DataType.FLOAT,
+        ),
+        (
+            [],
+            int,
+            'Int64',
+            audformat.define.DataType.INTEGER,
+        ),
+        (
+            [],
+            'int64',
+            'Int64',
+            audformat.define.DataType.INTEGER,
+        ),
+        (
+            [],
+            'Int64',
+            'Int64',
+            audformat.define.DataType.INTEGER,
+        ),
+        (
+            [],
+            str,
+            'object',
+            audformat.define.DataType.STRING,
+        ),
+        (
+            [],
+            'timedelta64[ns]',
+            'timedelta64[ns]',
+            audformat.define.DataType.TIME,
+        ),
+        (
+            [0],
+            'datetime64[ns]',
+            'datetime64[ns]',
+            audformat.define.DataType.DATE,
+        ),
+        (
+            [0.0],
+            None,
+            'float64',
+            audformat.define.DataType.FLOAT,
+        ),
+        (
+            [0],
+            None,
+            'Int64',
+            audformat.define.DataType.INTEGER,
+        ),
+        (
+            [np.NaN],
+            'Int64',
+            'Int64',
+            audformat.define.DataType.INTEGER,
+        ),
+        (
+            [0, np.NaN],
+            'Int64',
+            'Int64',
+            audformat.define.DataType.INTEGER,
+        ),
+        (
+            [np.NaN],
+            'Int64',
+            'Int64',
+            audformat.define.DataType.INTEGER,
+        ),
+        (
+            ['0'],
+            None,
+            'object',
+            audformat.define.DataType.STRING,
+        ),
+        (
+            [0],
+            'timedelta64[ns]',
+            'timedelta64[ns]',
+            audformat.define.DataType.TIME,
+        ),
+    ]
+)
+def test_dtype_multiindex(
+        tmpdir,
+        index_values,
+        index_dtype,
+        expected_pandas_dtype,
+        expected_audformat_dtype,
+):
+    expected_audformat_dtypes = [expected_audformat_dtype] * 2
+    expected_pandas_dtypes = [expected_pandas_dtype] * 2
+    index = pd.MultiIndex.from_arrays(
+        [
+            pd.Series(index_values, dtype=index_dtype),
+            pd.Series(index_values, dtype=index_dtype),
+        ],
+        names=['idx1', 'idx2'],
+
+    )
+    table = audformat.MiscTable(index)
+    assert list(table.levels.values()) == expected_audformat_dtypes
+    assert list(table.index.dtypes) == expected_pandas_dtypes
+
+    # Store and load table
+    db = audformat.testing.create_db(minimal=True)
+    db['misc'] = table
+    assert list(db['misc'].levels.values()) == expected_audformat_dtypes
+    assert list(db['misc'].index.dtypes) == expected_pandas_dtypes
+
+    db_root = tmpdir.join('db')
+    db.save(db_root, storage_format='csv')
+    db_new = audformat.Database.load(db_root)
+    assert list(db_new['misc'].levels.values()) == expected_audformat_dtypes
+    assert list(db_new['misc'].index.dtypes) == expected_pandas_dtypes
 
 
 @pytest.mark.parametrize(
