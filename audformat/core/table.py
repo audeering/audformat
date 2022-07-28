fed232825b1e8d0810e0023b3e7376803257dabe
Johannes Wagner <jwagner@audeering.com>
2022-07-28 17:09:22 +0200

    use utils.symmetric_difference() in drop_index()

--- audformat/core/table.py
+++ audformat/core/table.py
@@ -231,7 +231,7 @@ class Base(HeaderBase):
 
         _assert_table_index(self, index, 'drop rows from')
 
-        new_index = self.df.index.difference(index)
+        new_index = utils.symmetric_difference([self.index, index])
         self._df = self.df.reindex(new_index)
 
         return self
