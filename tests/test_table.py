436ac7daeb740dc40ad9ecae4b1d590d480b774b
Hagen Wierstorf <hwierstorf@audeering.com>
2022-08-03 12:55:12 +0200

    TST: [pick|extend|drop]_index() should preserve order (#254)

--- tests/test_table.py
+++ tests/test_table.py
@@ -547,6 +547,37 @@ def test_drop_and_pick_index():
         pytest.DB['files'].pick_index(index).get()
 
 
+def test_drop_extend_and_pick_index_order():
+
+    # Ensure order of index is preserved.
+    index = audformat.filewise_index(['f4', 'f3', 'f2', 'f1'])
+    table = audformat.Table(index)
+    # pick
+    new_table = table.pick_index(
+        audformat.filewise_index(['f1', 'f2'])
+    )
+    pd.testing.assert_index_equal(
+        new_table.index,
+        audformat.filewise_index(['f2', 'f1']),
+    )
+    # extend
+    new_table = table.extend_index(
+        audformat.filewise_index('f5')
+    )
+    pd.testing.assert_index_equal(
+        new_table.index,
+        audformat.filewise_index(['f4', 'f3', 'f2', 'f1', 'f5']),
+    )
+    # drop
+    new_table = table.drop_index(
+        audformat.filewise_index(['f1', 'f2'])
+    )
+    pd.testing.assert_index_equal(
+        new_table.index,
+        audformat.filewise_index(['f4', 'f3']),
+    )
+
+
 @pytest.mark.parametrize(
     'files',
     [
