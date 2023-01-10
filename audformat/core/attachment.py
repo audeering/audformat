b3f8125516e38cb56c615c8c4cf26df3c07bfe17
Hagen Wierstorf <hwierstorf@audeering.com>
2023-01-10 16:23:04 +0100

    Update audformat/core/attachment.py
    Co-authored-by: Johannes Wagner <jwagner@audeering.com>

--- audformat/core/attachment.py
+++ audformat/core/attachment.py
@@ -64,7 +64,7 @@ class Attachment(HeaderBase):
     def files(
             self,
     ) -> typing.List[str]:
-        r"""List all files part of the attachment.
+        r"""List all files that are part of the attachment.
 
         List recursively the relative path
         of all files that exist
