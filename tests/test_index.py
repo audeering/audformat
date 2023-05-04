ba1a0ed19380f3473b4e9d13f5439ca58dc6cc13
Hagen Wierstorf <hwierstorf@audeering.com>
2023-05-04 14:25:38 +0200

    Add failing test

--- tests/test_index.py
+++ tests/test_index.py
@@ -279,3 +279,12 @@ def test_create_segmented_index(files, starts, ends):
 )
 def test_index_type(index, index_type):
     assert audformat.index_type(index) == index_type
+
+
+def test_to_timedelta():
+    # Test precision of time values stored in segmented index,
+    # see https://github.com/audeering/audinterface/issues/113
+    duration = 2.5225
+    times = [duration]
+    times = audformat.core.index.to_timedelta(times)
+    assert times[0].total_seconds() == duration
