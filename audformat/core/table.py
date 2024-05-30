f161704e31e20476f0ca00a3e5e970657223765f
Hagen Wierstorf <hwierstorf@audeering.com>
2024-05-30 15:11:01 +0200

    Improve comment

--- audformat/core/table.py
+++ audformat/core/table.py
@@ -1072,8 +1072,8 @@ class MiscTable(Base):
             if isinstance(index, pd.MultiIndex) and index.nlevels == 1:
                 index = index.get_level_values(0)
 
-            # Ensure integers are always stored as Int64,
-            # and bool as boolean,
+            # Ensure integers are stored as Int64,
+            # and bool values as boolean,
             # compare audformat.core.common.to_pandas_dtype()
             index = utils._maybe_convert_pandas_dtype(index)
 
