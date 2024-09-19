ac67688b5e22e53a8735f310f99cab93e6896bf3
Hagen Wierstorf <hwierstorf@audeering.com>
2024-09-19 14:35:30 +0200

    Update audformat/core/utils.py
    Co-authored-by: ChristianGeng <christian.c.geng@gmail.com>

--- audformat/core/utils.py
+++ audformat/core/utils.py
@@ -1273,7 +1273,7 @@ def map_language(language: str) -> str:
 
     `ISO 639-3`_ is an international standard for language codes.
     It defines three-letter codes for identifying languages,
-    with an aim to cover all known natural languages.
+    with the aim to cover all known languages.
 
     .. _ISO 639-3: https://iso639-3.sil.org/code_tables/639/data/
 
