a387e3a618b8ad75add982bbc9c38a90ac03f396
Hagen Wierstorf <hwierstorf@audeering.com>
2026-01-23 11:40:24 +0100

    Update segmented_index()

--- audformat/core/index.py
+++ audformat/core/index.py
@@ -361,7 +361,14 @@ def segmented_index(
             define.IndexField.END,
         ],
     )
-    index = utils.set_index_dtypes(index, {define.IndexField.FILE: "string"})
+    index = utils.set_index_dtypes(
+        index,
+        {
+            define.IndexField.FILE: "string",
+            define.IndexField.START: "timedelta64[ns]",
+            define.IndexField.END: "timedelta64[ns]",
+        },
+    )
     assert_index(index)
 
     return index
